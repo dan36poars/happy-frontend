bf48c071c58b1bae42b8cfe85d910e7bd910bcec
daniel correa <eldestemido@gmail.com>
2023-11-17 18:47:22 -0300

    backend with fake data

--- public/js/page-orphanages.js
+++ public/js/page-orphanages.js
@@ -1,31 +1,42 @@
 // create map
 // coords = -30.031966,-51.197669
-const map = L.map("mapid").setView([-30.031966,-51.197669], 13);
+const map = L.map("mapid").setView([-30.031966, -51.197669], 13);
 
 // tileLayer Map added
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png")
-.addTo(map);
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
 // created icon
 const icon = L.icon({
-    iconUrl: "./images/map-mark.svg",
-    iconSize: [58, 68],
-    iconAnchor: [29, 68],
-    popupAnchor: [170, 2]
+  iconUrl: "./images/map-mark.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
 });
 
 // created popup overlay
-const popup = L.popup({
+function addMarker({ id, name, lat, lng }) {
+  const popup = L.popup({
     closeButton: false,
-    className: 'map-popup',
+    className: "map-popup",
     minWidth: 240,
-    minHeight: 240
-})
-.setContent('Pão dos pobres <a href="orphanage.html?id=1" class="choose-orphanage"><img src="./images/arrow-white.svg"/></a>');
+    minHeight: 240,
+  }).setContent(
+    `${name} <a href="./orphanage?id=${id}" class="choose-orphanage"><img src="./images/arrow-white.svg"/></a>`
+  );
 
-// Popup Mark
-L
-.marker([-30.031966,-51.197669], { icon })
-.addTo(map)
-.bindPopup(popup);
+  // Popup Mark
+  L.marker([lat, lng], { icon }).addTo(map).bindPopup(popup);
+}
 
+const orphanagesSpan = document.querySelectorAll(".data-orphanages span");
+
+orphanagesSpan.forEach((span) => {
+    const orphanage = {
+        id: span.dataset.id,
+        name: span.dataset.name,
+        lat: span.dataset.lat,
+        lng: span.dataset.lng,
+    }
+
+    addMarker(orphanage);
+});
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const pages = require("./pages.js");
 
 // starting express lib
 const server = express();
@@ -9,11 +10,10 @@ server
   .use(express.static("public")) // static pages like css, js, images
   .set("views", path.join(__dirname, "views")) // template engine setting
   .set("view engine", "hbs")
-  .get("/", (request, response) => {
-    console.log(__dirname);
-    const city = request.query.city;
-    return response.render("index", { city });
-  });
+  .get("/", pages.index )
+  .get("/orphanage", pages.orphanage )
+  .get("/orphanages", pages.orphanages )
+  .get("/create-orphanage", pages.create_orphanage );
 
 // listen
 server.listen(3000);
